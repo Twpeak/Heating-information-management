5e437d587003d064fd7a51865812d254df14e170
g_Twpeak <g_2914075814@qq.com>
2022-07-14 16:35:52 +0800

    修改报错

--- server/api/system/enter.go
+++ server/api/system/enter.go
@@ -10,16 +10,9 @@ type ApiGroup struct {
 }
 
 var (
-<<<<<<< HEAD
-	userService     = service.ServiceGroupApp.SystemServiceGroup.UserService
-	casbinService   = service.ServiceGroupApp.SystemServiceGroup.CasbinService
-	jwtService      = service.ServiceGroupApp.SystemServiceGroup.JwtService
-	hospitalService = service.ServiceGroupApp.SystemServiceGroup.HospitalService
-=======
 	userService 	= service.ServiceGroupApp.SystemServiceGroup.UserService
 	casbinService 	= service.ServiceGroupApp.SystemServiceGroup.CasbinService
 	jwtService   	= service.ServiceGroupApp.SystemServiceGroup.JwtService
 	hospitalService = service.ServiceGroupApp.SystemServiceGroup.HospitalService
 	feverService	= service.ServiceGroupApp.SystemServiceGroup.FeverService
->>>>>>> master
 )
